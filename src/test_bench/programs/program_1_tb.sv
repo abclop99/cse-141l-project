959e5836bdacf60bcca4284229b5112e9ee1e4b0
Aaron Li <a1li@ucsd.edu>
2023-03-08 13:52:14 -0800

    Randomize inputs in test bench

--- src/test_bench/programs/program_1_tb.sv
+++ src/test_bench/programs/program_1_tb.sv
@@ -29,6 +29,12 @@ module program_1_tb();
 		dut.dm1.core[0] = 8'b00000101;
 		dut.dm1.core[1] = 8'b01010101;
 
+		// Initialize rest of the messages as random
+		for (int i = 2; i < 30; i = i + 2) begin
+			dut.dm1.core[i] = $urandom_range(0, 7);
+			dut.dm1.core[i + 1] = $urandom_range(0, 255);
+		end
+
 		// Print input data [0:29]
 		$display("Input data [0:29]:");
 		$display("================");
@@ -45,12 +51,14 @@ module program_1_tb();
 		#20;
 
 		// Print output from data memory [30:59]
+		// And check the results
 		$display("Output from data memory [30:59]:");
 		$display("================================");
-		$display("  Address \tData");
-		$display("----------\t----");
-		for (int i = 30; i < 60; i = i + 1) begin
-			$display("%d\t%b", i, dut.dm1.core[i]);
+		$display("  Address \t  In Data \tOut Data \tParity?");
+		$display("----------\t----------\t--------");
+		for (int i = 0; i < 30; i = i + 2) begin
+			$display("> %d\t%b\t%b", i, dut.dm1.core[i], dut.dm1.core[i+30]);
+			$display("  %d\t%b\t%b", i+1, dut.dm1.core[i+1], dut.dm1.core[i+31]);
 		end
 
 		$stop;
