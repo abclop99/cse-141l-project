1d9f36168252be35c87f9716abe38e4550b2cb80
Aaron Li <a1li@ucsd.edu>
2023-03-14 23:55:23 -0700

    Program 3 pattern between bits with 0 for everything
    Still need to mask the message and more

--- src/assembly/program_3.asm
+++ src/assembly/program_3.asm
@@ -64,83 +64,182 @@ add	i 0	# 27
 
 #### Loop: (28 >> 2 = 7)
 
-### Match within byte; 4 tests
+	### Match within byte; 4 tests
 
-# Set inner loop index
-loadv	i 4	# 28
-storev	r 6
-# NOP
-add i 0
-add i 0
+	# Set inner loop index
+	loadv	i 4	# 28
+	storev	r 6
+	# NOP
+	add i 0
+	add i 0
 
-## Inner Loop: (32 >> 2 = 8 (can't branch in single instruction.))
+	## Inner Loop: (32 >> 2 = 8 (can't branch in single instruction.))
 
-# Test for pattern first time in byte
-# Current byte is already loaded in acc
-loadv	r 2
-xor	r 4	#	XOR with bit pattern
-beq	i 1	#	Skip skipping add if matches
-rb	i 3	#		Skip adding to count
-# Add to count
-loadv	r 1
-add	i 1
-storev	r 1
+		# Test for pattern first time in byte
+		# Current byte is already loaded in acc
+		loadv	r 2
+		xor	r 4	#	XOR with bit pattern
+		beq	i 1	#	Skip skipping add if matches
+		rb	i 3	#		Skip adding to count
+		# Add to count
+		loadv	r 1
+		add	i 1
+		storev	r 1
 
-# Decrement inner loop index; if !0, set&shift byte and loop
-loadv	r 6
-sub	i 1
-storev	r 6
-beq	i 7	#	If end of loop, skip looping
-# shift current byte
-loadv	r 2
-sl	i 1
-storev	r 2
-# ab to 8 << 2 = 32
-loadv	i 1
-sl	i 3
-storev	r 0
-ab	r 0
+		# Decrement inner loop index; if !0, set&shift byte and loop
+		loadv	r 6
+		sub	i 1
+		storev	r 6
+		beq	i 7	#	If end of loop, skip looping
+		# shift current byte
+		loadv	r 2
+		sl	i 1
+		storev	r 2
+		# ab to 8 << 2 = 32
+		loadv	i 1
+		sl	i 3
+		storev	r 0
+		ab	r 0
 
-## Update byte-wise counters
-# Add to occurences in byte (Mem[33])
-loadv	r 7	#	(32)
-add	i 1	#	(33)
-storev	r 0
-loadm	r 0	#	(Mem[33])
-add	r 1	#	Add count
-storem	r 0	#	Store back to Mem[33]
-# If count != 0: increment bytes with pattern (Mem[34])
-loadv	r 7	#	(32)
-add	i 2	#	(34)
-storev	r 0
-loadv	r 1	#	Load byte count
-xor	i 0	#	Compare with 0
-beq	i 3	#	Skip increment if 0
-loadm	r 0	#	Mem[34]
-add	i 1
-storem	r 0	#	Store back to Mem[34]
+	## Update byte-wise counters
+	# Add to occurences in byte (Mem[33])
+	loadv	r 7	#	(32)
+	add	i 1	#	(33)
+	storev	r 0
+	loadm	r 0	#	(Mem[33])
+	add	r 1	#	Add count
+	storem	r 0	#	Store back to Mem[33]
+	# If count != 0: increment bytes with pattern (Mem[34])
+	loadv	r 7	#	(32)
+	add	i 2	#	(34)
+	storev	r 0
+	loadv	r 1	#	Load byte count
+	xor	i 0	#	Compare with 0
+	beq	i 3	#	Skip increment if 0
+	loadm	r 0	#	Mem[34]
+	add	i 1
+	storem	r 0	#	Store back to Mem[34]
 
-## Increment data address/loop index
-loadv	r 5
-add	i 1
-storev	r 5
+	## Increment data address/loop index
+	loadv	r 5
+	add	i 1
+	storev	r 5
 
-## break if reached end of loop (no next byte)
-# if (r5 == r7): break (done)
-xor	r 7
-beq	i 1
-rb	i 1
-done	i 7
+	## break if reached end of loop (no next byte)
+	# if (r5 == r7): add to counter in memory and done
+	xor	r 7
+	beq	i 1
+	rb	i 7
+	# Add to Mem[35]
+	loadv	r 7	# Load and store address
+	add	i 3
+	storev	r 0
+	#
+	loadm	r 0	# Increment data mem at address
+	add	r 1
+	storem	r 0
+	done	i 7
 
-## Load next byte
-loadm	r 5
-storev	r 3
+	## Load next byte
+	loadm	r 5
+	storev	r 3
 
-# TODO: Check between bytes
+	## Test for byte pattern across byte boundaries
+	# 4 tests, unrolled loop (easier this way)
 
-## Reset counter to 0
-loadv	i 0
-storev	r 1
+		## First test
+		#          |76543|210
+		# 0:       | 7654|321
+		# 1: 765432|1    |
+		loadv	r 2	# Load, shift, and store next byte
+		sr	i 1
+		storev	r 0
+		#
+		loadv	r 3	# Load and shift current byte
+		sl	i 7
+		or	r 0	# Combine with shifted next byte
+		xor	r 4	# Compare with pattern
+		#
+		beq	i 1	# Skip skipping if match
+		rb	i 3	#	Skip adding
+		#
+		loadv	r 1	# count++
+		add	i 1
+		storev	r 1
 
-# Branch to beginning of loop
-ab	i 7
+		## Second test
+		#         |76543|210
+		# 0:      |  765|4321
+		# 1: 76543|21   |
+		loadv	r 2	# Load, shift, and store next byte
+		sr	i 2
+		storev	r 0
+		#
+		loadv	r 3	# Load and shift current byte
+		sl	i 6
+		or	r 0	# Combine with shifted next byte
+		xor	r 4	# Compare with pattern
+		#
+		beq	i 1	# Skip skipping if match
+		rb	i 3	#	Skip adding
+		#
+		loadv	r 1	# count++
+		add	i 1
+		storev	r 1
+
+		## Third test
+		#        |76543|210
+		# 0:     |   76|54321
+		# 1: 7654|321  |
+		loadv	r 2	# Load, shift, and store next byte
+		sr	i 3
+		storev	r 0
+		#
+		loadv	r 3	# Load and shift current byte
+		sl	i 5
+		or	r 0	# Combine with shifted next byte
+		xor	r 4	# Compare with pattern
+		#
+		beq	i 1	# Skip skipping if match
+		rb	i 3	#	Skip adding
+		#
+		loadv	r 1	# count++
+		add	i 1
+		storev	r 1
+
+		## Fourth test
+		#       |76543|210
+		# 0:    |    7|654321
+		# 1: 765|4321 |
+		loadv	r 2	# Load, shift, and store next byte
+		sr	i 4
+		storev	r 0
+		#
+		loadv	r 3	# Load and shift current byte
+		sl	i 4
+		or	r 0	# Combine with shifted next byte
+		xor	r 4	# Compare with pattern
+		#
+		beq	i 1	# Skip skipping if match
+		rb	i 3	#	Skip adding
+		#
+		loadv	r 1	# count++
+		add	i 1
+		storev	r 1
+
+		## Update counter in memory
+		# Add to Mem[35]
+		loadv	r 7	# Load and store address
+		add	i 3
+		storev	r 0
+		#
+		loadm	r 0	# Increment data mem at address
+		add	r 1
+		storem	r 0
+
+	## Reset counter to 0
+	loadv	i 0
+	storev	r 1
+
+	# Branch to beginning of loop
+	ab	i 7
